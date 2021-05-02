13ae875f5cfdf88b6d15dede2b2f6a950a0c1d0c
Warlord56x <danika200151@gmail.com>
2021-05-02 15:49:12 +0200

    serialized json, fixed typo

--- cron.py
+++ cron.py
@@ -2,12 +2,13 @@ import requests as req
 import json
 
 steam = "http://api.steampowered.com"
-# Returns all the "games" (apps) from steam including the dlcs and other types
+# Returns all the "games" (apps) from steam including the dlcs and other types into a file
 def get_all_games():
     """Returns all games from steam to a file"""
     all_games = "/ISteamApps/GetAppList/v2"
     r = req.get(steam+all_games).json()
+    serialize = json.dumps(r, indent = 4)
     with open("games.json", "w") as jcron:
-        json.dump(r, jcron)
+        jcron.write(serialize)
 
 get_all_games()
